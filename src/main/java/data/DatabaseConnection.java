575a5d0642b3c480c4bfd395a458de1e9ba9ac73
erik--vink <Egali5te>
2016-10-06 11:35:37 +0200

    Initial commit

---
+++ src/main/java/data/DatabaseConnection.java
@@ -0,0 +1,26 @@
+package data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private String jdbcURL = "jdbc:oracle:thin:@//localhost:1521/XE";
+    private String userID = "erik";
+    private String password = "oracle";
+    private Connection connection;
+
+    public DatabaseConnection(){
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+            connection = DriverManager.getConnection(this.jdbcURL, this.userID, this.password);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}

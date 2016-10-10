a7f9e90212aadd8cbc354913852cbe161d85a0de
erik--vink <erik.vink95@gmail.com>
2016-10-10 23:22:41 +0200

    Added course import

--- src/main/java/data/helpers/CourseReader.java
+++ src/main/java/data/helpers/CourseReader.java
@@ -2,13 +2,120 @@ package data.helpers;
 
 import data.domain.Course;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by Erik on 10-10-2016.
  */
 public class CourseReader {
 
-    public static Course stringToCourse(){
-        return null;
+    public static ArrayList<Course> stringToCourses(String input) throws InvalidPropertiesFormatException {
+
+        ArrayList<Course> courses = new ArrayList<>();
+
+        String[] coursePairs = input.split("\r\n\r\n");
+
+        for(String coursepair: coursePairs){
+            String[] keyValuePairs = coursepair.split("\r\n");
+            LinkedHashMap<String, String> map = new LinkedHashMap<>();
+
+            if(keyValuePairs.length < 4){
+                throw new InvalidPropertiesFormatException("The file is missing fields");
+            }
+
+            if(keyValuePairs.length > 4){
+                throw new InvalidPropertiesFormatException("The file is missing new lines");
+            }
+
+            for(String s : keyValuePairs){
+                String[] keyValueParts = s.split(":");
+                for (int i = 0; i < keyValueParts.length; i += 2) {
+                    map.put(keyValueParts[i], keyValueParts[i + 1]);
+                }
+            }
+            courses.add(parseMapToCourse(map));
+        }
+
+        return courses;
+    }
+
+    private static Course parseMapToCourse(LinkedHashMap<String, String> map) throws InvalidPropertiesFormatException {
+        Course course = new Course();
+        int currentPosition = 0;
+
+        for(String key : map.keySet()){
+            switch(key){
+                case "Cursuscode":
+                    checkOrderPosition(key, currentPosition);
+                    course.setCode(map.get(key).trim());
+                    break;
+                case "Startdatum":
+                    checkOrderPosition(key, currentPosition);
+                    String date = map.get(key).trim();
+                    String datePattern = "[0-9]{2}\\/[0-9]{2}\\/[0-9]{4}";
+                    Pattern r = Pattern.compile(datePattern);
+                    Matcher dateMatcher = r.matcher(date);
+                    if (dateMatcher.find( )) {
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                        course.setStartDate(LocalDate.parse(date, formatter));
+                    }
+                    else{
+                        throw new InvalidPropertiesFormatException("Invalid date format");
+                    }
+                    break;
+                case "Titel":
+                    checkOrderPosition(key, currentPosition);
+                    course.setTitle(map.get(key).trim());
+                    break;
+                case "Duur":
+                    checkOrderPosition(key, currentPosition);
+                    String durationString = map.get(key).trim();
+                    String durationPattern = "[0-9]{1,2} dagen";
+                    Pattern dp = Pattern.compile(durationPattern);
+                    Matcher durationMatcher = dp.matcher(durationString);
+                    if (durationMatcher.find( )) {
+                        int duration = Integer.valueOf(durationString.substring(0, durationString.indexOf(" ")));
+                        course.setDuration(duration);
+                    }
+                    else{
+                        throw new InvalidPropertiesFormatException("Invalid duration format");
+                    }
+                    break;
+            }
+            currentPosition++;
+        }
+
+        return course;
+    }
+
+    private static void checkOrderPosition(String key, int position) throws InvalidPropertiesFormatException {
+        String errorMessage = "Invalid order format";
+        switch (key){
+            case "Titel":
+                if(position != 0){
+                    throw new InvalidPropertiesFormatException(errorMessage);
+                }
+                break;
+            case "Cursuscode":
+                if(position != 1){
+                    throw new InvalidPropertiesFormatException(errorMessage);
+                }
+                break;
+            case "Duur":
+                if(position != 2){
+                    throw new InvalidPropertiesFormatException(errorMessage);
+                }
+                break;
+            case "Startdatum":
+                if(position != 3){
+                    throw new InvalidPropertiesFormatException(errorMessage);
+                }
+                break;
+        }
     }
 
 }
--- src/test/test/data/domain/CourseTest.java
+++ src/test/test/data/domain/CourseTest.java
@@ -1,12 +1,19 @@
 package data.domain;
 
 import data.controllers.CourseController;
+import data.helpers.CourseReader;
 import data.repositories.DummyCourseRepository;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import org.junit.rules.ExpectedException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -16,6 +23,10 @@ import static org.hamcrest.Matchers.containsInAnyOrder;
  * Created by Erik on 10-10-2016.
  */
 public class CourseTest {
+
+    @Rule
+    public ExpectedException thrown= ExpectedException.none();
+
 
     @BeforeClass
     public static void initialize(){
@@ -60,4 +71,71 @@ public class CourseTest {
         assertThat(courseController.getCourseById(1), is(course1));
     }
 
+    @Test
+    public void importValidCourseFileReturnsSingleCourse() throws IOException {
+
+        String text = Files.toString(new File("src/test/resources/correctSingleCourse.txt"), Charsets.UTF_8);
+        ArrayList<Course> courses = CourseReader.stringToCourses(text);
+        Course course = TestBuilders.getCourse().title("C# Programmeren").code("CNETIN").startDate(LocalDate.parse("2013-10-14")).duration(5).build();
+
+        assertThat(courses, containsInAnyOrder(course));
+    }
+
+    @Test
+    public void importValidCourseFileReturnsMultipleCourses() throws IOException {
+        String text = Files.toString(new File("src/test/resources/correctMultipleCourses.txt"), Charsets.UTF_8);
+        ArrayList<Course> courses = CourseReader.stringToCourses(text);
+        Course course1 = TestBuilders.getCourse().title("C# Programmeren").code("CNETIN").startDate(LocalDate.parse("2013-10-14")).duration(5).build();
+        Course course2 = TestBuilders.getCourse().title("C# Programmeren").code("CNETIN").startDate(LocalDate.parse("2013-10-21")).duration(5).build();
+        Course course3 = TestBuilders.getCourse().title("Advanced C#").code("ADCSB").startDate(LocalDate.parse("2013-10-21")).duration(2).build();
+        Course course4 = TestBuilders.getCourse().title("Advanced C#").code("ADCSB").startDate(LocalDate.parse("2013-10-14")).duration(2).build();
+        Course course5 = TestBuilders.getCourse().title("C# Programmeren").code("CNETIN").startDate(LocalDate.parse("2013-10-14")).duration(5).build();
+
+        assertThat(courses, containsInAnyOrder(course1, course2, course3, course4, course5));
+    }
+
+    @Test
+    public void importCourseFileWithMissingFieldsReturnsError() throws IOException {
+        String text = Files.toString(new File("src/test/resources/missingFields.txt"), Charsets.UTF_8);
+
+        thrown.expectMessage("missing fields");
+
+        CourseReader.stringToCourses(text);
+    }
+
+    @Test
+    public void importCourseFileWithMissingNewLinesReturnsError() throws IOException {
+        String text = Files.toString(new File("src/test/resources/missingNewLines.txt"), Charsets.UTF_8);
+
+        thrown.expectMessage("missing new lines");
+
+        CourseReader.stringToCourses(text);
+    }
+
+    @Test
+    public void importCourseFileWithWrongDateFormatReturnsError() throws IOException {
+        String text = Files.toString(new File("src/test/resources/wrongDateFormat.txt"), Charsets.UTF_8);
+
+        thrown.expectMessage("Invalid date format");
+
+        CourseReader.stringToCourses(text);
+    }
+
+    @Test
+    public void importCourseFileWithWrongDurationFormatReturnsError() throws IOException {
+        String text = Files.toString(new File("src/test/resources/wrongDurationFormat.txt"), Charsets.UTF_8);
+
+        thrown.expectMessage("Invalid duration format");
+
+        CourseReader.stringToCourses(text);
+    }
+
+    @Test
+    public void importCourseFileWithWrongOrderReturnsError() throws IOException {
+        String text = Files.toString(new File("src/test/resources/wrongOrder.txt"), Charsets.UTF_8);
+
+        thrown.expectMessage("Invalid order format");
+
+        CourseReader.stringToCourses(text);
+    }
 }
